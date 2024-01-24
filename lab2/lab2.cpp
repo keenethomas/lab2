cdaf72e132cd5390333b52ed768ed00f339511ce
keenet <keenet@X-KNOY306PC06>
2024-01-24 12:15:08 -0500

    Lab2

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -3,9 +3,38 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    float A;
+    float B;
+    float X;
+
+    cout << "Hello, my name is Thomas and I'm going to solve the equation:" << endl;
+    cout << "Ax + B = 0" << endl;
+    cout << "For x" << endl;
+
+    cout << " " << endl;
+
+    cout << "Please enter a value for A: ";
+    cin >> A;
+
+    cout << "Please enter a value for B: ";
+    cin >> B;
+
+    cout << " " << endl; 
+
+    cout << "Solving" << A << "x+" << B << "=0 for x..." << endl; 
+
+    cout << " " << endl; 
+
+    X = (-B/A);
+   
+
+    cout << "The answer is:" << endl; 
+        cout << "x=" << X << endl;
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
